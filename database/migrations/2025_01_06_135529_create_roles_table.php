0e7c1884d6cf5f8d5625077c393a7e5fa59d9f65
robzdd1922 <robipermanaofficial@gmail.com>
2025-01-06 21:07:56 +0700

    rob

--- database/migrations/2025_01_06_135529_create_roles_table.php
+++ database/migrations/2025_01_06_135529_create_roles_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
+            $table->string('customers');
+            $table->string('admin');
             $table->timestamps();
         });
     }
