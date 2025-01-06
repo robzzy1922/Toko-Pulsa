0c6602fb7e9f68af7400fd5701b42f0940cc05fd
robzdd1922 <robipermanaofficial@gmail.com>
2025-01-06 21:21:13 +0700

    robi_cell

--- database/migrations/2025_01_06_135607_create_products_table.php
+++ database/migrations/2025_01_06_135607_create_products_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->enum('type', ['pulsa', 'kuota', 'token listrik']);
+            $table->integer('price');
+            $table->integer('stock');
+            $table->string('description');
+            $table->string('created_at');
+            $table->string('updated_at');
             $table->timestamps();
         });
     }
--- database/migrations/2025_01_06_135627_create_detail_transactions_table.php
+++ database/migrations/2025_01_06_135627_create_detail_transactions_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('detail_transactions', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('id_transactions')->constrained('transactions')->onDelete('cascade');
+            $table->foreignId('id_products')->constrained('products')->onDelete('cascade');
+            $table->string('description');
+            $table->integer('quantity');
+            $table->integer('price');
+            $table->integer('subtotal');
             $table->timestamps();
         });
     }
