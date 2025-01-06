ebe882cdf8bb40e29fe83a110fc571078f74fc42
robzdd1922 <robipermanaofficial@gmail.com>
2025-01-06 21:14:11 +0700

    robi_cell

--- database/migrations/2025_01_06_135548_create_transactions_table.php
+++ database/migrations/2025_01_06_135548_create_transactions_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('id_users')->constrained('users')->onDelete('cascade');
+            $table->foreignId('id_products')->constrained('products')->onDelete('cascade');
+            $table->integer('phone_number');
+            $table->integer('amount');
+            $table->enum('status', ['pending', 'success', 'failed']);
+            $table->string('payment_method');
+            $table->integer('total');
             $table->timestamps();
         });
     }
