93db1fcdf51bf44dafec5e6d930a2a950e2d32ab
robzdd1922 <robipermanaofficial@gmail.com>
2025-01-07 17:23:13 +0700

    robi_cell

--- app/Models/App_setting.php
+++ app/Models/App_setting.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class App_setting extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'app_settings';
+
+    protected $fillable = [
+        'key',
+        'value'
+    ];
 }
--- app/Models/Carts.php
+++ app/Models/Carts.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Carts extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'carts';
+
+    protected $fillable = [
+        'product_id',
+        'quantity',
+        'user_id'
+    ];
 }
