57795d828da79efb95b1a8efd0e8d0bc69cabe2d
robzdd1922 <robipermanaofficial@gmail.com>
2025-01-07 10:01:22 +0700

    robi_cell

--- app/Models/Detail_transaction.php
+++ app/Models/Detail_transaction.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Detail_transaction extends Model
 {
-    //
+    protected $table = 'detail_transactions';
+
+    protected $fillable = [
+        'id_transaction',
+        'id_product',
+        'description',
+        'quantity',
+        'price',
+        'subtotal'
+    ];
 }
