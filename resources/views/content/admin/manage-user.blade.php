fd4a56a140049813dc094eec52a671eb5bd3d4bf
robzdd1922 <robipermanaofficial@gmail.com>
2025-01-09 12:10:34 +0700

    robi_cell

--- resources/views/content/admin/manage-user.blade.php
+++ resources/views/content/admin/manage-user.blade.php
@@ -1,63 +1,63 @@
 @extends('layout.admin.main')
 @section('content')
-<div class="mt-0 main-content app-content">
-    <div class="side-app">
+    <div class="mt-0 main-content app-content">
+        <div class="side-app">
 
-        <!-- CONTAINER -->
-        <div class="main-container container-fluid">
+            <!-- CONTAINER -->
+            <div class="main-container container-fluid">
 
-            <!-- PAGE-HEADER -->
-            <div class="page-header">
-                <h1 class="page-title">Table</h1>
-                <div>
-                    <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="javascript:void(0)">Tables</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">Table</li>
-                    </ol>
+                <!-- PAGE-HEADER -->
+                <div class="page-header">
+                    <h1 class="page-title">Table</h1>
+                    <div>
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="javascript:void(0)">Tables</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Table</li>
+                        </ol>
+                    </div>
                 </div>
-            </div>
-            <!-- PAGE-HEADER END -->
+                <!-- PAGE-HEADER END -->
 
 
 
-            <!-- Row -->
-            <div class="row">
-                <div class="col-xl-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <h3 class="card-title">Users</h3>
-                        </div>
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <table class="table mb-2 border text-nowrap text-md-nowrap table-hover">
-                                    <thead class="table-primary">
-                                        <tr>
-                                            <th>ID</th>
-                                            <th>Username</th>
-                                            <th>Email</th>
-                                            <th>Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
+                <!-- Row -->
+                <div class="row">
+                    <div class="col-xl-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Users</h3>
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table class="table mb-2 border text-nowrap text-md-nowrap table-hover">
+                                        <thead class="table-primary">
+                                            <tr>
+                                                <th>ID</th>
+                                                <th>Username</th>
+                                                <th>Email</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
 
-                                    </tbody>
-                                </table>
+                                        </tbody>
+                                    </table>
 
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
+                <!-- End Row -->
             </div>
-            <!-- End Row -->
-        </div>
-        <!-- CONTAINER CLOSED -->
+            <!-- CONTAINER CLOSED -->
 
+        </div>
     </div>
-</div>
 @endsection
 
 @section('scripts')
-<script src="assets/plugins/datatable/js/jquery.dataTables.min.js"></script>
+    <script src="assets/plugins/datatable/js/jquery.dataTables.min.js"></script>
     <script src="assets/plugins/datatable/js/dataTables.bootstrap5.js"></script>
     <script src="assets/plugins/datatable/js/dataTables.buttons.min.js"></script>
     <script src="assets/plugins/datatable/js/buttons.bootstrap5.min.js"></script>
@@ -69,5 +69,32 @@
     <script src="assets/plugins/datatable/js/buttons.colVis.min.js"></script>
     <script src="assets/plugins/datatable/dataTables.responsive.min.js"></script>
     <script src="assets/plugins/datatable/responsive.bootstrap5.min.js"></script>
-    <script src="assets/js/table-data.js"></script>
+    <script>
+        $('#example').DataTable({
+            serverSide: true,
+            ajax: {
+                url: "{{ route('user-datatable') }}",
+                type: 'POST'
+            },
+            columnsDefs: [{
+                targets: 0,
+                render: function(data, type, full, meta) {
+                    return (meta.row + 1)
+                }
+            }],
+            columns: [{
+                    data: 'id'
+                },
+                {
+                    data: 'username'
+                },
+                {
+                    data: 'email'
+                },
+                {
+                    data: 'id'
+                }
+            ]
+        });
+    </script>
 @endsection
